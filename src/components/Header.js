3c21eb112b2ab5fea07e9edbdab2a26015685f1d
Jake Wehder <jw@email.comjake>
2022-09-29 16:51:22 -0500

    commit tree ef7c9ce150580ce910a48d3127751df84b679076

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Header({ onDarkModeClick, modeState }) {
+    return (
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>{modeState ? "Dark" : "Light"} Mode</button>
+        </header>
+    )  
+}
+
+export default Header;
